aa8896faee924be9c6177abe812978cd642f7632
João Vítor Guimarães Douverny <77338020+Joao-VitorGD1703@users.noreply.github.com>
2024-09-23 22:17:19 -0300

    feat: register user

--- web/src/composables/User.js
+++ web/src/composables/User.js
@@ -19,4 +19,26 @@ export async function getUser(emails, passwords) {
         console.error('Error fetching user:', error.message);
     }
 }
+export async function postUser(emails, passwords, nome) {
+    //padrão NÃO DELETAR
+    const email = emails
+    const password = passwords
+    //const email = "j@j.com"
+    //const password = "123"
+    try {
+        const response = await axios.post('http://localhost:8080/user/register', 
+        {
+            name: nome,
+            role: 'RH',
+            email: email,
+            password: password
+
+        });
+        console.log('Status:', response.status);
+        console.log('Data:', response.data);
+        return response.data
+    } catch (error) {
+        console.error('Error fetching user:', error.message);
+    }
+}
 
